3709a6b95c9eef563768dfe7e0cd3362155f2a25
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-03-12 23:33:31 -0300

    gsap/7scrollmagic

--- app.js
+++ app.js
@@ -15,10 +15,184 @@ hamMenuClose.addEventListener("click", () => {
     xBtn.classList.toggle("op");
 })
 
-//Parallax effect//
+//Gsap Animation landing Page //
 
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
+const tl = new TimelineMax();
 
+tl.from(".landing-img-1", 2,
+    {
+        opacity: 0,
+        ease: Power2.easeInOut
+    })
+    .from(".landing-img-2", 2,
+        {
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=2")
+    .from(".intro-title-1", 1.5,
+        {
+            x: -10,
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=.5")
+    .from(".intro-title-2", 1.5,
+        {
+            x: 10,
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=1.5")
+    .from(".intro-subtitle", 1.5,
+        {
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=1")
+    .from("#nav", 1,
+        {
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=1.5")
+    .from(".logo", 1,
+        {
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=1.2")
+    .from(".location-dir", 1.5,
+        {
+            y: 50,
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=1.2")
+    .from(".location-day", 1.5,
+        {
+            y: 50,
+            opacity: 0,
+            ease: Power2.easeInOut
+        }, "-=1.1")
 
+//ScrollMagic Animations//
+const controller = new ScrollMagic.Controller();
 
+//-------------------Scene-1-------------------//
+const tl2 = new TimelineMax();
+tl2.from(".wording-age", 2,
+    {
+        y: 20,
+        ease: Power2.easeInOut
+    })
+
+let scene1 = new ScrollMagic.Scene({
+    triggerElement: ".wording-age",
+    triggerHook: 1,
+    offset: 300
+})
+    .setTween(tl2)
+    .addIndicators()
+    .addTo(controller);
+
+//-------------------Scene-2-------------------//
+const tl3 = new TimelineMax();
+tl2.from(".menu-intro-img-vert", 2,
+    {
+        opacity: 0,
+        ease: Power2.easeIn
+    })
+tl2.from(".menu-intro-img-hor", 2,
+    {
+        opacity: 0,
+        ease: Power2.easeIn
+    }, "-=2")
+tl2.from(".menu-intro-text", 2,
+    {
+        y: 20,
+        ease: Power2.easeIn
+    }, "-=2.5")
+
+let scene2 = new ScrollMagic.Scene({
+    triggerElement: ".menu-intro",
+    triggerHook: 1,
+    offset: "200"
+})
+    .setTween(tl3)
+    .addIndicators()
+    .addTo(controller);
+
+//-------------------Scene-3-------------------//
+const tl4 = new TimelineMax();
+tl4.from(".menu-info", 1.5,
+    {
+        y: 50,
+        ease: Power2.easeInOut
+    })
+
+let scene3 = new ScrollMagic.Scene({
+    triggerElement: ".menu-info",
+    triggerHook: 1,
+    offset: 300
+})
+    .setTween(tl4)
+    .addIndicators()
+    .addTo(controller);
+
+//-------------------Scene-4-------------------//
+const tl5 = new TimelineMax();
+tl5.from(".about-img-1", 1,
+    {
+        x: -30,
+        ease: Power2.easeIn
+    })
+tl5.from(".about-img-2", 1,
+    {
+        x: 30,
+        ease: Power2.easeIn
+    }, "-=1")
+tl5.from(".about-intro", 1,
+    {
+        y: 20,
+        ease: Power2.easeIn
+    }, "-=.5")
+
+let scene4 = new ScrollMagic.Scene({
+    triggerElement: ".about",
+    triggerHook: 1,
+    offset: "300"
+})
+    .setTween(tl5)
+    .addIndicators()
+    .addTo(controller);
+
+//-------------------Scene-5-------------------//
+const tl6 = new TimelineMax();
+tl6.from(".feed", 1,
+    {
+        y: 100,
+        ease: Power2.easeInOut
+    })
+
+let scene5 = new ScrollMagic.Scene({
+    triggerElement: ".feed",
+    triggerHook: 1,
+    offset: 500
+
+})
+    .setTween(tl6)
+    .addIndicators()
+    .addTo(controller);
+
+//-------------------Scene-6-------------------//
+
+const tl7 = new TimelineMax();
+tl7.from(".contact", 1,
+    {
+        y: 100,
+        ease: Power2.easeInOut
+    })
+
+let scene6 = new ScrollMagic.Scene({
+    triggerElement: ".contact",
+    triggerHook: 1,
+    offset: 300
+
+})
+    .setTween(tl7)
+    .addIndicators()
+    .addTo(controller);
