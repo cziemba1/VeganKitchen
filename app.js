aff3caf067bfc6b5333dadd9f602bd590f7b80d5
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-03-12 21:27:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//Toggle Menu//
+let hamMenuOpen = document.querySelector(".toggle-btn");
+let hamMenuClose = document.querySelector(".toggle-btn-primary")
+let nav = document.querySelector("#nav");
+let xBtn = document.querySelector(".menu-strip");
+
+
+hamMenuOpen.addEventListener("click", () => {
+    nav.classList.toggle("menu-close");
+    xBtn.classList.remove("op");
+})
+
+hamMenuClose.addEventListener("click", () => {
+    nav.classList.toggle("menu-close");
+    xBtn.classList.toggle("op");
+})
+
+//Parallax effect//
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+
+
